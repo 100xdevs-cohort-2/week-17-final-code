9f574261fa2b431fbb20e64cefc647b4c34dfc7b
sahil <sahilcodes23@gmail.com>
2024-07-08 03:49:08 +0530

    new comit

--- apps/merchant-app/lib/auth.ts
+++ apps/merchant-app/lib/auth.ts
@@ -1,8 +1,7 @@
 import GoogleProvider from "next-auth/providers/google";
-import { NextAuthOptions } from "next-auth";
 import db from "@repo/db/client";
 
-export const authOptions: NextAuthOptions = {
+export const authOptions = {
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID || "",
@@ -10,35 +9,40 @@ export const authOptions: NextAuthOptions = {
         })
     ],
     callbacks: {
-      async signIn({ user, account }) {
+      async signIn({ user, account }: {
+        user: {
+          email: string;
+          name: string
+        },
+        account: {
+          provider: "google" | "github"
+        }
+      }) {
+        console.log("hi signin")
         if (!user || !user.email) {
           return false;
         }
 
-        try {
-          await db.merchant.upsert({
-            where: {
-              email: user.email
-            },
-            create: {
-              email: user.email,
-              name: user.name || "",
-              auth_type: account.provider === "google" ? "Google" : "Github"
-            },
-            update: {
-              name: user.name || "",
-              auth_type: account.provider === "google" ? "Google" : "Github"
-            },
-            select: {
-              id: true
-            }
-          });
-          return true;
-        } catch (error) {
-          console.error("Error during sign-in:", error);
-          return false;
-        }
+        await db.merchant.upsert({
+          select: {
+            id: true
+          },
+          where: {
+            email: user.email
+          },
+          create: {
+            email: user.email,
+            name: user.name,
+            auth_type: account.provider === "google" ? "Google" : "Github" // Use a prisma type here
+          },
+          update: {
+            name: user.name,
+            auth_type: account.provider === "google" ? "Google" : "Github" // Use a prisma type here
+          }
+        });
+
+        return true;
       }
     },
     secret: process.env.NEXTAUTH_SECRET || "secret"
-};
+  }

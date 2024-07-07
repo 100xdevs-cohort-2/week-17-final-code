9e4b028ca0464200a277eb98105a686fbcac983a
sahil <sahilcodes23@gmail.com>
2024-07-08 02:48:22 +0530

    aasda

--- apps/merchant-app/app/page.tsx
+++ apps/merchant-app/app/page.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import { useBalance } from "@repo/store/balance";
-
 export default function() {
-  const balance = useBalance();
+  
   return <div>
-    hi there {balance}
+    hi there {}
   </div>
 }
